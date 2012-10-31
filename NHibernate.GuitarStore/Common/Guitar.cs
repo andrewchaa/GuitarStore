f3214c0b78f4762a97a9415a2243c0ec35aaf0dc
Andrew Chaa <andrew.chaa@huddle.com>
2012-10-31 13:48:46 +0000

    initial commit

---
+++ NHibernate.GuitarStore/Common/Guitar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.GuitarStore.Common
+{
+    public class Guitar
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Type { get; set; }
+
+        public IList<Inventory> Inventory { get; set; }
+    }
+}
