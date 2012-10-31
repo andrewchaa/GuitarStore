9e2c4a988f2a5ff0435a1831bf9423dc1086cb4d
Andrew Chaa <andrew.chaa@huddle.com>
2012-10-31 17:37:30 +0000

    adding classes

---
+++ NHibernate.GuitarStore/DataAccess/NHibernateBase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+
+namespace NHibernate.GuitarStore.DataAccess
+{
+    public class NHibernateBase
+    {
+        protected static ISessionFactory SessionFactory { get; set; }
+
+        private static Configuration Configuration { get; set; }
+        private static ISession _session = null;
+        private static IStatelessSession _statelessSession = null;
+
+        public static Configuration ConfigureNHibernate(string assembly)
+        {
+            Configuration = new Configuration();
+            Configuration.AddAssembly(assembly);
+
+            return Configuration;
+        }
+
+        public void Initialize(string assembly)
+        {
+            Configuration = ConfigureNHibernate(assembly);
+            SessionFactory = Configuration.BuildSessionFactory();
+        }
+
+        public static ISession Session
+        {
+            get
+            {
+                if (_session == null)
+                {
+                    _session = SessionFactory.OpenSession();
+                }
+                
+                return _session;
+            }
+        }
+
+        public static IStatelessSession StatelessSession
+        {
+            get
+            {
+                if (_statelessSession == null)
+                {
+                    _statelessSession = SessionFactory.OpenStatelessSession();
+                }
+
+                return _statelessSession;
+            }
+        }
+    }
+}
